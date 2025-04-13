c527b0dce6bc981b2293b0f69c55801a167cda00
HJ0311 <hyehan@c2r6s2.42seoul.kr>
2025-04-13 17:35:44 +0900

    feat: google 연동 관련 버튼 추가

--- Frontend/src/pages/Login/Login.tsx
+++ Frontend/src/pages/Login/Login.tsx
@@ -1,14 +1,11 @@
 import { useState } from "react"
 import Container from "./components/Container"
 import MainTitle from "./components/MainTitle"
-import InputField from "./components/InputField"
-import ActionButton from "./components/ActionButton"
 import Credit from "./components/Credit"
+import ActionButton from "./components/ActionButton"
 import ErrorMessage from "./components/ErrorMessage"
 
 const Login = () => {
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
 	const [error, setError] = useState("")
 
 	return (
@@ -19,24 +16,8 @@ const Login = () => {
 			<div className="absolute left-1/2 -translate-x-1/2 top-[10px]">
 				<ErrorMessage message={error} setError={setError}/>
 			</div>
-			<div className="flex flex-col items-center justify-center space-y-[26px] absolute left-[200px] top-[270px]">
-				<InputField
-					label="EMAIL"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					type="text"
-					width="250px"
-				/>
-				<InputField
-					label="PW"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					type="password"
-					width="250px"
-				/>
-			</div>
-			<div className="absolute left-1/2 -translate-x-1/2 top-[370px]">
-				<ActionButton email={email} password={password} setError={setError}/>
+			<div className="absolute left-1/2 -translate-x-1/2 top-[320px]">
+			<ActionButton setError={setError}/>
 			</div>
 			<div className="absolute left-1/2 -translate-x-1/2 w-[630px] bottom-[35px]">
 				<Credit />
