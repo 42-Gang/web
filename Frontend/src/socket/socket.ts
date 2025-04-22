d2bac12b82b99257731d6d0e10d364ad84ab8fad
yeni023 <yeeun3011@naver.com>
2025-04-22 10:42:44 +0900

    chore: socket.ts 주석 추가

--- Frontend/src/socket/socket.ts
+++ Frontend/src/socket/socket.ts
@@ -5,8 +5,9 @@ export const socket = io(import.meta.env.VITE_BACKEND_SOCKET_URL, {
   path: "/socket.io",
   transports: ["websocket", "polling"],
 
-  // 최신 access token을 socket 연결마다 자동 반영
+  // 최신 access token을 socket 연결마다 자동 반영 (token-auth로 전달중)
   // cb : callback
+  // WebSocket의 인증용 handshake시 서버로 전달
   auth: (cb) => {
     cb({ token: getAccessToken() });
   },
