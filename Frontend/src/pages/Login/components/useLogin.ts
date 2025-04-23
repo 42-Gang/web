541b0306a37fdba248191ac7d5b62abf68a62e1e
HJ0311 <hyehan@c2r6s2.42seoul.kr>
2025-04-23 15:30:16 +0900

    fix: space 간격 수정

--- Frontend/src/pages/Login/components/useLogin.ts
+++ Frontend/src/pages/Login/components/useLogin.ts
@@ -2,51 +2,51 @@
 import { useNavigate } from "react-router-dom"
 
 interface LoginResponse {
-	code: number
-	message: string
-	data: {
-		accessToken: string
-	}
+  code: number
+  message: string
+  data: {
+    accessToken: string
+  }
 }
 
 const useLogin = (setError: (msg: string) => void) => {
-	const navigate = useNavigate()
+  const navigate = useNavigate()
 
-	const login = async (email: string, password: string) => {
-		try {
-			const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/login`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify({ email, password })
-			})
+  const login = async (email: string, password: string) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      })
 
-			const body: LoginResponse = await response.json()
+      const body: LoginResponse = await response.json()
 
-			// 로그인 성공
-			if (response.status === 200) {
-				console.log ("Login successful: ", body)
-				// Access Token 저장
-				localStorage.setItem("accessToken", body.data?.accessToken || "")
-				// Home 페이지로 이동
-				navigate('/Home')
-				return
-			}
+      // 로그인 성공
+      if (response.status === 200) {
+        console.log ("Login successful: ", body)
+        // Access Token 저장
+        localStorage.setItem("accessToken", body.data?.accessToken || "")
+        // Home 페이지로 이동
+        navigate('/Home')
+        return
+      }
 
-			// 서버에서 넘겨주는 에러 메세지 그대로 출력
-			if (body.message) {
-				setError(body.message)
-			} else {
-				setError("An unexpected error occurred. Please try again.")
-			}
-		} catch (error) {
-			console.error("Login error:", error)
-			setError("An unexpected error occurred. Please try again.")
-		}
-	}
+      // 서버에서 넘겨주는 에러 메세지 그대로 출력
+      if (body.message) {
+        setError(body.message)
+      } else {
+        setError("An unexpected error occurred. Please try again.")
+      }
+    } catch (error) {
+      console.error("Login error:", error)
+      setError("An unexpected error occurred. Please try again.")
+    }
+  }
 
-	return login
+  return login
 }
 
 export default useLogin
