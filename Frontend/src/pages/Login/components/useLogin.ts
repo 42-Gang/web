950bb548e4f67a14913a40ca66dd673e11a99eaa
HJ0311 <hyehan@c2r6s2.42seoul.kr>
2025-04-23 15:22:05 +0900

    fix: Login error message 출력 방식 수정

--- Frontend/src/pages/Login/components/useLogin.ts
+++ Frontend/src/pages/Login/components/useLogin.ts
@@ -23,29 +23,22 @@ const useLogin = (setError: (msg: string) => void) => {
 			})
 
 			const body: LoginResponse = await response.json()
+
+			// 로그인 성공
 			if (response.status === 200) {
-				console.log("Login successful:", body)
-				// 로그인 성공 시 accessToken 저장
+				console.log ("Login successful: ", body)
+				// Access Token 저장
 				localStorage.setItem("accessToken", body.data?.accessToken || "")
-
-				// 바로 Home 페이지로 이동
-				navigate("/Home")
+				// Home 페이지로 이동
+				navigate('/Home')
 				return
 			}
 
-			if (response.status === 400) {
-				setError("Please check your email or password.")
-				return
-			}
-
-			if (response.status === 401) {
-				setError("The email or password is incorrect.")
-				return
-			}
-
-			if (response.status === 500) {
-				setError("There was a server error, please try again.")
-				return
+			// 서버에서 넘겨주는 에러 메세지 그대로 출력
+			if (body.message) {
+				setError(body.message)
+			} else {
+				setError("An unexpected error occurred. Please try again.")
 			}
 		} catch (error) {
 			console.error("Login error:", error)
