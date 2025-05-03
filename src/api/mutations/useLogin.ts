b94ff186b2f72584ef67bfff88abd5cb2bb564a2
Hak Lee <me@haklee.me>
2025-05-03 20:26:07 +0900

    build: update eslint rule

--- src/api/mutations/useLogin.ts
+++ src/api/mutations/useLogin.ts
@@ -1,12 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { fetcher } from '@/api';
+import { HttpResponse } from '@/api/types';
 
 type Request = {
   email: string;
   password: string;
 };
 
-const postLogin = (body: Request) => fetcher.post<void>('auth/login', { json: body });
+type Response = {
+  accessToken: string;
+};
+
+const postLogin = (body: Request) =>
+  fetcher.post<HttpResponse<Response>>('auth/login', { json: body });
 
 export const useLogin = () => useMutation({ mutationFn: postLogin });
--- src/pages/SignInPage/EmailLoginPage/index.tsx
+++ src/pages/SignInPage/EmailLoginPage/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useLogin } from '@/api';
+import { useAuthAtom } from '@/atoms/useAuthAtom';
 import { Flex } from '@/components/system';
 import { Branding, DefaultStepNavigator, GameLicense } from '@/components/ui';
 
@@ -10,14 +11,18 @@ export const EmailSignInPage = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const { setToken } = useAuthAtom();
 
-  const { mutateAsync: loginMutation } = useLogin();
+  const { mutateAsync } = useLogin();
 
   const handleSelect = async (index: number) => {
     switch (index) {
-      case 0:
-        await loginMutation({ email, password });
+      case 0: {
+        const { data } = await mutateAsync({ email, password });
+        if (!data) throw new Error('로그인에 실패했습니다.');
+        setToken(data.accessToken);
         break;
+      }
       case 1:
         navigate(-1);
         break;
@@ -29,6 +34,8 @@ export const EmailSignInPage = () => {
   return (
     <Flex direction="column" justifyContent="space-between" style={{ height: '100%' }}>
       <Branding css={css({ marginTop: '74px' })} />
+
+      {/* TODO: Implement UI */}
       <Flex direction="column" justifyContent="center">
         <Flex justifyContent="center" style={{ color: 'white' }}>
           <p>EMAIL : </p>
@@ -38,6 +45,7 @@ export const EmailSignInPage = () => {
           <p>PW : </p>
           <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
         </Flex>
+
         <DefaultStepNavigator
           style={{ marginTop: '12px' }}
           items={['CONTINUE', 'GO BACK']}
