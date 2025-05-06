1bfa8afc66870df914336691e75d5f3db1109fec
Hak Lee <me@haklee.me>
2025-05-06 16:09:22 +0900

    feat(route): add game, friend, profile route constants

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,42 @@
-import { Branding } from '@/components/ui';
+import { css } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
+
+import { Flex } from '@/components/system';
+import { Branding, DefaultStepNavigator, GameLicense } from '@/components/ui';
+import { PATH } from '@/constants';
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSelect = (index: number) => {
+    switch (index) {
+      case 0:
+        navigate(PATH.SIGNIN_EMAIL);
+        break;
+      case 1:
+        navigate(PATH.SIGNIN_EMAIL);
+        break;
+      case 2:
+        navigate(-1);
+        break;
+      case 3:
+        navigate(-1);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <Branding />
+      <Flex direction="column" justifyContent="space-between" style={{ height: '100%' }}>
+        <Branding css={css({ marginTop: '74px' })} />
+        <DefaultStepNavigator
+          items={['START GAME', 'GAME HISTORY', 'FRIEND', 'PROFILE']}
+          onSelect={handleSelect}
+        />
+        <GameLicense />
+      </Flex>
     </>
   );
 };
