d6205a06680626d20a828214b9f456318749861c
Hyehan <124490845+HJ0311@users.noreply.github.com>
2025-05-08 17:45:18 +0900

    feat: tournament 승률 서버로부터 요청 받을 수 있도록 API 구현 (#121)
    Co-authored-by: HJ0311 <hyehan@c2r6s2.42seoul.kr>

--- src/pages/HistoryPage/components/go-home-page/index.tsx
+++ src/pages/HistoryPage/components/go-home-page/index.tsx
@@ -1,23 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import { Interpolation, Theme } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
 
 import { PATH } from '@/constants';
 
 import * as styles from './styles.ts';
 
-type GoHomePageProps = {
-  css?: Interpolation<Theme>;
-};
-
-export const GoHomePage = ({ css }: GoHomePageProps) => {
+export const GoHomePage = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate(PATH.HOME);
   };
 
-  return (
-    <styles.Button css={css} onClick={handleClick} aria-label="Go to home page"></styles.Button>
-  );
+  return <styles.Button onClick={handleClick} aria-label="Go to home page" />;
 };
--- src/pages/HistoryPage/components/view-toggle/index.tsx
+++ src/pages/HistoryPage/components/view-toggle/index.tsx
@@ -1,18 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import { Interpolation, Theme } from '@emotion/react';
-
 import * as styles from './styles.ts';
 
 type ViewToggleProps = {
   label: string;
   onClick: () => void;
   isSelected: boolean;
-  css?: Interpolation<Theme>;
 };
 
-export const ViewToggle = ({ label, onClick, isSelected, css }: ViewToggleProps) => {
+export const ViewToggle = ({ label, onClick, isSelected }: ViewToggleProps) => {
   return (
-    <styles.Button css={css} onClick={onClick} data-selected={isSelected}>
+    <styles.Button onClick={onClick} data-selected={isSelected}>
       <span>{label}</span>
     </styles.Button>
   );
