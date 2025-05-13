cc62053e321ebd491b4ee92cb96e8ccb58206b95
HJ0311 <hyehan@c1r4s1.42seoul.kr>
2025-05-13 20:49:33 +0900

    feat: 코드 수정

--- src/pages/FriendPage/components/friend-list/index.tsx
+++ src/pages/FriendPage/components/friend-list/index.tsx
@@ -1,15 +1,18 @@
 import type { Friend } from '@/api/types';
 
 // import { useFriendsMe } from "@/api";
-
 import { FriendItem } from './friend-item';
 import * as styles from './styles.css';
+
 type FriendListProps = {
   friends: Friend[];
 };
+
 export const FriendList = ({ friends }: FriendListProps) => {
   // const { data } = useFriendsMe();
+
   // const friends = data?.data?.friends || [];
+
   return (
     <div className={styles.friendListWrapper}>
       {friends.map((friend) => (
