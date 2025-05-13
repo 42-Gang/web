102f23c7861d0e77cbd5e3fe514108bb549cb1ce
HJ0311 <hyehan@c1r4s1.42seoul.kr>
2025-05-13 20:33:46 +0900

    fix: 삭제된 코드 복원

--- src/pages/FriendPage/components/friend-list/index.tsx
+++ src/pages/FriendPage/components/friend-list/index.tsx
@@ -1,19 +1,19 @@
-import type { Friend } from '@/api/types';
-
-// import { useFriendsMe } from "@/api";
-import { FriendItem } from './friend-item';
 import * as styles from './styles.css';
 
-type FriendListProps = {
-  friends: Friend[];
-};
+const FRIENDS = ['PING', 'JACK', 'DING', 'PONG'];
 
-export const FriendList = ({ friends }: FriendListProps) => {
+export const FriendList = () => {
   return (
-    <div className={styles.friendListWrapper}>
-      {friends.map((friend) => (
-        <FriendItem key={friend.friendId} friend={friend} />
-      ))}
+    <div className={styles.friendList}>
+      <ul className={styles.list}>
+        <li className={styles.divider} />
+        {FRIENDS.map((friend) => (
+          <li key={friend} className={styles.item}>
+            <img src="/assets/images/sample-avatar.png" className={styles.avatar} />
+            <span>{friend}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
