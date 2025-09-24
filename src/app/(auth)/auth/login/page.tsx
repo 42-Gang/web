2c7d6e63d0f220b28f2b1515aa491511f1773061
ohprettyhak <me@haklee.me>
2025-09-24 12:03:58 +0900

    feat: create login page component with BrandTitle integration

---
+++ src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { BrandTitle } from '~/components/ui';
+
+const Page = () => {
+  return (
+    <div className="column-between">
+      <BrandTitle />
+    </div>
+  );
+};
+
+export default Page;
