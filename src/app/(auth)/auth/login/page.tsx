7db570c80fb49695f0cc24f11536140a156b75cb
ohprettyhak <me@haklee.me>
2025-10-04 16:38:01 +0900

    feat: enhance MenuSelector component and integrate into page layout

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -1,9 +1,18 @@
-import { BrandTitle } from '~/components/ui';
+import { BrandFooter, BrandTitle, MenuSelector } from '~/components/ui';
+import { routes } from '~/constants/routes';
 
 const Page = () => {
   return (
-    <div className="column-between">
+    <div className="column-between h-full">
       <BrandTitle />
+
+      <MenuSelector>
+        <MenuSelector.Link href={`/${routes.login}`}>SIGN IN WITH GOOGLE</MenuSelector.Link>
+        <MenuSelector.Link href={`/${routes.register}`}>SIGN IN WITH EMAIL</MenuSelector.Link>
+        <MenuSelector.Button>GO BACK</MenuSelector.Button>
+      </MenuSelector>
+
+      <BrandFooter />
     </div>
   );
 };
--- src/components/ui/menu-selector.tsx
+++ src/components/ui/menu-selector.tsx
@@ -6,7 +6,12 @@ import { QuinqueFive } from '~/app/_fonts';
 interface MenuSelectorRootProps extends ComponentProps<'div'> {}
 
 const MenuSelectorRoot = ({ className, ...props }: MenuSelectorRootProps) => {
-  return <div className={twMerge('column gap-3', QuinqueFive.className, className)} {...props} />;
+  return (
+    <div
+      className={twMerge('column-center-x gap-3', QuinqueFive.className, className)}
+      {...props}
+    />
+  );
 };
 
 interface MenuSelectorLinkProps extends ComponentProps<typeof Link> {}
@@ -15,7 +20,7 @@ const MenuSelectorLink = ({ className, ...props }: MenuSelectorLinkProps) => {
   return (
     <Link
       className={twMerge(
-        '-tracking-[0.1rem] relative cursor-pointer text-white text-xl',
+        '-tracking-[0.1rem] relative w-fit cursor-pointer text-white text-xl',
         'before:-left-8 before:-translate-y-1/2 before:absolute before:top-1/2 before:text-2xl before:opacity-0 before:transition-all before:duration-200 before:content-["▶"]',
         'before:translate-x-[-4px] hover:before:translate-x-0 hover:before:opacity-100',
         className,
@@ -31,7 +36,7 @@ const MenuSelectorButton = ({ className, ...props }: MenuSelectorButtonProps) =>
   return (
     <button
       className={twMerge(
-        '-tracking-[0.1rem] relative cursor-pointer text-white text-xl',
+        '-tracking-[0.1rem] relative w-fit cursor-pointer text-white text-xl',
         'before:-left-8 before:-translate-y-1/2 before:absolute before:top-1/2 before:text-2xl before:opacity-0 before:transition-all before:duration-200 before:content-["▶"]',
         'before:translate-x-[-4px] hover:before:translate-x-0 hover:before:opacity-100',
         className,
