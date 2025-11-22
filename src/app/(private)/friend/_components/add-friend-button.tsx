d5bd86888a6044447dea5d5896dae56d310c9b01
kungbi <kipper12335@gmail.com>
2025-11-22 23:51:45 +0900

    refactor(friend-page): Replace AddFriendDialog with AddFriendButton for improved UI interaction

---
+++ src/app/(private)/friend/_components/add-friend-button.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { twMerge } from 'tailwind-merge';
+import { UsersIcon } from '~/components/icon';
+import { AddFriendDialog } from './add-friend-dialog';
+
+export const AddFriendButton = () => {
+  return (
+    <AddFriendDialog>
+      <button type="button" className={twMerge('shrink-0 cursor-pointer text-white')}>
+        <UsersIcon size={35} />
+      </button>
+    </AddFriendDialog>
+  );
+};
--- src/app/(private)/friend/page.tsx
+++ src/app/(private)/friend/page.tsx
@@ -1,9 +1,8 @@
 import { Suspense } from '@suspensive/react';
 import { twMerge } from 'tailwind-merge';
 import { SuperPixel } from '~/app/_fonts';
-import { UsersIcon } from '~/components/icon';
 import { CloseButton } from '~/components/ui';
-import { AddFriendDialog } from './_components/add-friend-dialog';
+import { AddFriendButton } from './_components/add-friend-button';
 import { FriendFilterInput } from './_components/friend-filter-input';
 import { FriendList } from './_components/friend-list';
 import { FriendRequestButton } from './_components/friend-request-button';
@@ -20,17 +19,7 @@ const Page = () => {
         <div className="center relative mt-6 w-full">
           <div className="center-y w-[75%] gap-2">
             <Suspense clientOnly>
-              <AddFriendDialog>
-                <button
-                  type="button"
-                  className={twMerge(
-                    'center size-11 shrink-0 cursor-pointer rounded-2xl border-2 border-neutral-50 text-white',
-                    'hover:bg-neutral-50/20 active:translate-y-px',
-                  )}
-                >
-                  <UsersIcon size={24} />
-                </button>
-              </AddFriendDialog>
+              <AddFriendButton />
             </Suspense>
             <Suspense>
               <FriendFilterInput />
